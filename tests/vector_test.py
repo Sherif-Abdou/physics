6f36c38c238cc61242b38d7000dab5c8fc8a7c7f
Sherif Abdou <sherif_abdou@outlook.com>
2020-03-13 18:54:46 -0700

    Expanded Testing for Vector

--- tests/vector_test.py
+++ tests/vector_test.py
@@ -1,6 +1,6 @@
 import unittest
 from src.vector import *
-from math import sqrt
+from math import sqrt, radians
 
 
 class VectorTests(unittest.TestCase):
@@ -11,8 +11,18 @@ class VectorTests(unittest.TestCase):
     def test_magnitude(self):
         vector = Vector(1, 1)
         self.assertEqual(vector.mag, sqrt(2))
+        self.assertAlmostEqual(vector.angle, radians(45), delta=0.01)
         vector.mag *= 2
-        self.assertAlmostEqual(vector.mag, sqrt(8))
+        self.assertEqual(vector.mag, sqrt(8))
+        self.assertAlmostEqual(vector.angle, radians(45), delta=0.01)
+
+    def test_angle(self):
+        vector = Vector(1, 1)
+        self.assertEqual(vector.mag, sqrt(2))
+        self.assertAlmostEqual(vector.angle, radians(45), delta=0.01)
+        vector.angle = vector.angle / 2
+        self.assertEqual(vector.mag, sqrt(2))
+        self.assertAlmostEqual(vector.angle, radians(45)/2, delta=0.01)
 
 if __name__ == '__main__':
     unittest.main()
