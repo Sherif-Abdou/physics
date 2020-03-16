5195c6c14b19567eff4333f3d3cdfab378a2bc15
Sherif Abdou <sherif_abdou@outlook.com>
2020-03-16 08:30:05 -0700

    Fixed Newton

--- src/units/newton.py
+++ src/units/newton.py
@@ -4,24 +4,25 @@ from .vector import Vector
 class Newton:
     def __init__(self):
         self.acceleration = Vector(0, 0)
-        self.__mass = 0
+        self.__mass: float = 0
 
     @classmethod
     def from_vector(cls, vector, mass):
         value = cls()
+        value.acceleration = vector
         value.acceleration /= mass
         value.__mass = mass
         return value
 
-    def to_vector(self):
+    def to_vector(self) -> Vector:
         return self.acceleration * self.mass
 
     @property
-    def mass(self):
+    def mass(self) -> float:
         return self.__mass
 
     @mass.setter
-    def mass(self, value):
+    def mass(self, value: float):
         full_acc = self.acceleration * self.mass
         self.acceleration = full_acc / value
         self.mass = value
